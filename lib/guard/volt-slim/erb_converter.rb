c1af63d4e176e97816f76925dd19f4aad244def3
Андрей Большов <bolshov@uniweb.ru>
2015-08-20 19:58:50 +0300

    disable custom replacer

--- lib/guard/volt-slim/erb_converter.rb
+++ lib/guard/volt-slim/erb_converter.rb
@@ -2,7 +2,7 @@ module Guard
   class VoltSlim
     class ERBConverter < ::Slim::ERBConverter
       replace :'Slim::CodeAttributes', Guard::VoltSlim::Filters::CodeAttributes
-      before :'Guard::VoltSlim::Filters::CodeAttributes', Guard::VoltSlim::Filters::AttrValueConverter
+      # before :'Guard::VoltSlim::Filters::CodeAttributes', Guard::VoltSlim::Filters::AttrValueConverter
       replace :AttributeMerger, Guard::VoltSlim::Filters::AttributeMerger
       replace :Pretty, Guard::VoltSlim::Filters::Pretty
       replace :'Slim::Controls', Guard::VoltSlim::Filters::Controls
